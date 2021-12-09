972ce42c4f26f6e553782e859085f0b1e3012062
Arthur Scarpati <arthur_159@hotmail.com>
2021-12-09 14:22:15 -0300

    Enable H2, Role and users entities

--- src/main/java/com/ForumApi/entities/Role.java
+++ src/main/java/com/ForumApi/entities/Role.java
@@ -1,6 +1,9 @@
 package com.ForumApi.entities;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -14,6 +17,8 @@ import lombok.Setter;
 @Setter
 public class Role {
 
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String authority;
+
 }
--- src/main/java/com/ForumApi/entities/User.java
+++ src/main/java/com/ForumApi/entities/User.java
@@ -1,9 +1,16 @@
 package com.ForumApi.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -25,4 +32,9 @@ public class User {
 	private String firstName;
 	private String lastName;
 	
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(name = "tb_user_role",
+	joinColumns = @JoinColumn(name = "user_id"),
+	inverseJoinColumns = @JoinColumn(name = "role_id"))
+	private List<Role> roles = new ArrayList<>();
 }
