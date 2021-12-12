f8ef99a10527f2f5d49c09742bc1091526e1db49
Arthur Scarpati <arthur_159@hotmail.com>
2021-12-12 15:12:37 -0300

    Update user Controller

--- src/main/java/com/ForumApi/controller/UserController.java
+++ src/main/java/com/ForumApi/controller/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -54,6 +55,12 @@ public class UserController {
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
 				.buildAndExpand(newDto.getId()).toUri();
 		return ResponseEntity.created(uri).body(newDto);
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<UserDTO> update(@PathVariable Long id, @RequestBody UserInsertDTO dto){
+		UserDTO updatedDto = service.update(id, dto);
+		return ResponseEntity.ok().body(updatedDto);
 	}
 	
 }
