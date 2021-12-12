634970d9d57fa774e4f72c821e0ada7e544335ba
Arthur Scarpati <arthur_159@hotmail.com>
2021-12-12 15:06:29 -0300

    Insert User Controller

--- src/main/java/com/ForumApi/controller/UserController.java
+++ src/main/java/com/ForumApi/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.ForumApi.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,11 +8,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.ForumApi.dto.UserDTO;
 import com.ForumApi.dto.UserDTOLessDetail;
+import com.ForumApi.dto.UserInsertDTO;
 import com.ForumApi.exceptions.UserNotFoundException;
 import com.ForumApi.services.UserService;
 
@@ -43,6 +48,12 @@ public class UserController {
 		return ResponseEntity.noContent().build();
 	}
 	
-	
+	@PostMapping
+	public ResponseEntity<UserDTO> insert(@RequestBody UserInsertDTO dto) {
+		UserDTO newDto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(newDto.getId()).toUri();
+		return ResponseEntity.created(uri).body(newDto);
+	}
 	
 }
