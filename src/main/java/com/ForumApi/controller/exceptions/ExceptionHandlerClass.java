7b1418fd80e1bff57b44d59261e5ef5593535c7c
Arthur Scarpati <arthur_159@hotmail.com>
2022-01-13 19:36:58 -0300

    Exception handler

---
+++ src/main/java/com/ForumApi/controller/exceptions/ExceptionHandlerClass.java
@@ -0,0 +1,27 @@
+package com.ForumApi.controller.exceptions;
+
+import java.time.Instant;
+
+import javax.persistence.EntityNotFoundException;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionHandlerClass {
+
+	@ExceptionHandler(EntityNotFoundException.class)
+	public ResponseEntity<StandardError> entityNotFound(EntityNotFoundException e, HttpServletRequest request) {
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError();
+		err.setTimestamp(Instant.now());
+		err.setStatus(status.value());
+		err.setError("Resource not found");
+		err.setMessage(e.getMessage());
+		err.setPath(request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+}
